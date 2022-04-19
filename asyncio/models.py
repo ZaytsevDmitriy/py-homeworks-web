ddf70fdb6fc97fb0dc889562bd2a6b7bbb349055
Dmitriy <dmitrijzaytsev85@gmail.com>
2022-04-19 19:24:18 +0300

    v.0

--- asyncio/models.py
+++ asyncio/models.py
@@ -1,6 +1,9 @@
 import aiohttp
+import asyncio
 from sqlalchemy import create_engine, Column, Integer, String, Date
 from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.util import asyncio
+from datetime import datetime
 
 engine = create_engine('postgresql://admin:1234@127.0.0.1:5432/asyncio_netology')
 Base = declarative_base()
@@ -26,13 +29,44 @@ class Person_model(Base):
     vehicles = Column(String)
 
 
-    some_string = " ".join(flexiple))
 
-async def get_info(url -> string:
+
+async def get_info(url) -> dict:
     async with aiohttp.ClientSession() as session:
-        async with session.get(f'{API_ENDPOINT}') as response:
+        async with session.get(f'{url}') as response:
             response = await response.json()
             return response
 
+
+async def info_list(url):
+    result = await asyncio.gather(*[get_info(i) for i in url])
+
+    result =  ", ".join([i.get('title') for i in result])
+    return result
+
+async def save_person_in_db(p_data):
+    async with Session() as session:
+        async with session.begin():
+            for person in p_data:
+                birth_year = datetime.strptime(person['birth_year'])
+                eye_color = person['eye_color']
+                films = asyncio.run(info_list(person['films']))
+                gender = person['gender']
+                hair_color = person['hair_color']
+                height = int(person['eye_color'])
+                homeworld = person['homeworld']
+                mass = int(person['mass'])
+                name = person['name']
+                skin_color = person['skin_color']
+                species = asyncio.run(info_list(person['species']))
+                starships = asyncio.run(info_list(person['starships']))
+                vehicles = asyncio.run(info_list(person['vehicles']))
+                person = Person_model(birth_year=birth_year, eye_color=eye_color, films=films, gender=gender,
+                                      hair_color=hair_color, height=height, homeworld=homeworld, mass=mass,
+                                      name=name, skin_color=skin_color, species=species, starships=starships,
+                                      vehicles=vehicles, person=person
+                                      )
+                session.add(person)  # добавляем данные юзера в сессию
+                session.commit()
 
 Base.metadata.create_all(engine)
